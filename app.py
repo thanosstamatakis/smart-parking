3efd974da3a9e3c87f1c0bb72569a3885edb36d2
gtsoulos <george.tsoulos@nokia.com>
2018-12-04 18:33:38 +0200

    Fix missing import

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from webmaps import app
 from config import CONFIGURATION
+from flask import Flask
 
 APP = Flask(__name__)
-APP.secret_key = CONFIGURATION.secret_key
+# APP.secret_key = CONFIGURATION.secret_key
 APP.config['SERVER_NAME'] = CONFIGURATION.get_server_name()
 
 if __name__ == '__main__':
--- webmaps/routes.py
+++ webmaps/routes.py
@@ -2,13 +2,16 @@ from flask import render_template, url_for, flash, redirect, request
 from webmaps import app, bcrypt
 from flask_login import login_user, logout_user, current_user, login_required
 
+
 @app.route("/")
 def landing():
     return render_template("landing.html")
 
+
 @app.route("/home")
 def home():
     return render_template("home.html", title='Home')
+
 
 @app.route("/about")
 def about():
