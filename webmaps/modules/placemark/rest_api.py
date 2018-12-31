94c37bb204a9f2ddac3ea6f245fb3ad2b3064ffc
gTsoulos <giorgos577@gmail.com>
2018-12-31 15:34:40 +0200

    Add post function to placemark to enable user to submit kml files.
    New post function to placemark api expects file object and returns
    according message if file upload was successful.

--- webmaps/modules/placemark/rest_api.py
+++ webmaps/modules/placemark/rest_api.py
@@ -1,9 +1,14 @@
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource, Namespace, reqparse
 from . import lib
 import flask
+import werkzeug
 
 NAMESPACE = Namespace(
     'placemark', description='Api namespace representing a placemark.')
+
+parser = NAMESPACE.parser()
+parser.add_argument('kml-file', type=werkzeug.datastructures.FileStorage,
+                    help='Kml file to be parsed', location='files', required=True)
 
 
 @NAMESPACE.route('/')
@@ -16,7 +21,24 @@ class Placemark(Resource):
         """ Return all placemark objects """
         response = []
         response = lib.get_placemark_objects()
-        
+
         # Response
         response = flask.jsonify(response)
+
+        return response
+
+    @NAMESPACE.expect(parser)
+    def post(self):
+        """
+        Input a new kml file and store information to db.
+        """
+
+        kml_file = parser.parse_args()['kml-file']
+
+        # Response
+        response = "File parsing was unsuccessful."
+        if kml_file:
+            file_name = werkzeug.utils.secure_filename(kml_file.filename)
+            response = f'File {file_name} was successfuly parsed.'
+
         return response
