c5cb430a17cd88d80fa83fa1b6c8a86f817d4ae8
thanosstamatakis <thanos.stamatakis@gmail.com>
2019-01-17 23:12:33 +0200

    Send KML to rest API and get back the percentage of the upload.

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -15,36 +15,4 @@ export class DataService {
     // return this.http.get('http://127.0.0.1:8080/languages/api/json')
     return this.http.get('http://localhost:8080/api/placemark/')
   }
-
-  // public postFile(fileToUpload: File): {[filekey:string]:Observable<number>} {
-  //   const endpoint = 'http://localhost:8080/api/placemark/';
-  //   const formData: FormData = new FormData();
-  //   const progress = new Subject<number>();
-  //   const status = {};
-
-  //   formData.append('fileKey', fileToUpload, fileToUpload.name);
-
-  //   const req = new HttpRequest('POST', endpoint, formData, {
-  //     reportProgress: true
-  //   });
-
-  //   this.http.request(req).subscribe(event => {
-  //     if (event.type === HttpEventType.UploadProgress) {
-  //       const percentDone = Math.round(100 * event.loaded / event.total);
-  //       progress.next(percentDone);
-  //     } else if (event instanceof HttpResponse) {
-  //       progress.complete();
-  //     }
-  //   });
-
-  //   status[fileToUpload.name] = {
-  //     progress: progress.asObservable()
-  //   };
-
-  //   return status;
-  //   // return this.http.post(endpoint, formData, { headers: yourHeadersConfig })
-  //   //   .map(() => { return true; })
-  //   //   .catch((e) => this.handleError(e));
-  // }
-
 }
--- frontend/src/app/report/report.component.ts
+++ frontend/src/app/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpProgressEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  
+  selectedFile: File = null;
 
-  constructor() { }
+  upload_msg: string = "";
+
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+    console.log(this.selectedFile);
+  }
+
+  onUpload(){
+    let formData = new FormData();
+    formData.append('kml-file', this.selectedFile, this.selectedFile.name);
+    console.log(formData);
+    this.http.post('http://localhost:8080/api/placemark/', formData, 
+    {reportProgress: true,
+    observe: 'events'})
+    .subscribe((event) => {
+      if (event.type === HttpEventType.UploadProgress) {
+          const percentDone = Math.round(100 * event.loaded / event.total);
+          const upload_msg = `${percentDone}% Uploaded`;
+          console.log(typeof(upload_msg));
+      }
+    }, (error) => {
+      console.log('Error', error);
+    });
+
+  }
+
+  constructor(private http: HttpClient) { }
   
   ngOnInit() {
     
