1a2d5cca8d21985aa23a19e9f18134f98d5b7fca
gtsoulos <george.tsoulos@nokia.com>
2019-01-22 17:08:32 +0200

    Test commit. Add to API test values for testing parking slots clustering.

--- backend/webmaps/modules/parkingslot/rest_api.py
+++ backend/webmaps/modules/parkingslot/rest_api.py
@@ -1,6 +1,12 @@
-from flask_restplus import Resource, Namespace
-from . import lib
+""" Parkingslots api module. """
+# Python mail libs.
+import numpy as np
+# Python libs.
 import flask
+from flask_restplus import Resource, Namespace
+# Project files.
+from . import lib
+from webmaps.models.slots_cluster import ParkingSlotsCluster
 
 NAMESPACE = Namespace(
     'parking-slot', description='Api namespace representing a parking slot.')
@@ -11,12 +17,16 @@ class ParkingSlot(Resource):
     """
     Api class placemark representing a parking-slot.
     """
-    @NAMESPACE.param('longitude ', 'Longitude of parking slot')
-    @NAMESPACE.param('latitude', 'Latitude of parking slot')
+    # @NAMESPACE.param('longitude ', 'Longitude of parking slot')
+    # @NAMESPACE.param('latitude', 'Latitude of parking slot')
+
     def get(self):
         """ Return all available parking slots """
-        response = []
-
+        X = np.array([[1, 2], [2, 2], [2, 3],
+                      [8, 7], [8, 8], [25, 80]])
+        parking_slot_cluster = ParkingSlotsCluster(X, [1, 3])
+        clusters = parking_slot_cluster.get_clusters()
+        response = clusters
         # Response
         response = flask.jsonify(response)
 
