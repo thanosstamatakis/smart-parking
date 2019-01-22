f8ed0bd1851a165ada438276c2a928b65c73fc19
gtsoulos <george.tsoulos@nokia.com>
2019-01-22 17:04:53 +0200

    Finish slots cluster model.

--- backend/webmaps/models/slots_cluster.py
+++ backend/webmaps/models/slots_cluster.py
@@ -1,10 +1,97 @@
 """ This module represents a Parking Slots Cluster model. """
+# Python libs.
+import logging
+import math
 from sklearn.cluster import DBSCAN
+from shapely.geometry import MultiPoint
+# Project files.
+from config import CONFIGURATION
+
+LOGGER = CONFIGURATION.get_logger(__name__)
 
 
 class ParkingSlotsCluster():
     """ Parking Slots Cluster Class. """
 
-    def __init__(self, parking_slots):
+    def __init__(self, parking_slots, user_location):
         """ Constructor method. """
         self.parking_slots = parking_slots
+        self.user_location = user_location
+        self.clustering = self.get_clustering()
+
+    def get_clustering(self):
+        """
+        Use DBSCAN to group points into clusters and
+        return clustering.
+        """
+        # Get DBSCAN model.
+        model = DBSCAN(eps=3, min_samples=2)
+        # Get clustering from DB scan model.
+        clustering = model.fit(self.parking_slots)
+
+        return clustering
+
+    def get_clusters(self):
+        """
+        Get all maximum parking slot clusters with their
+        attributes.
+        """
+        correct_clusters = list()
+        # Find number of clusters.
+        number_of_clusters = len(set(self.clustering.labels_)) - \
+            (1 if -1 in self.clustering.labels_ else 0)
+        # Get cluster arrays.
+        clusters = [self.parking_slots[self.clustering.labels_ == i]
+                    for i in range(number_of_clusters)]
+        # Get max points clusters.
+        clusters = self._find_max_point_cluster(clusters)
+
+        for cluster in clusters:
+            cluster_cent = self._get_clusters_centroid(cluster)
+            cluster_dist = self._get_clusters_distance(cluster_cent)
+            correct_clusters.append(
+                {'centroid': cluster_cent, 'distance': cluster_dist})
+
+        return correct_clusters
+
+    def _find_max_point_cluster(self, clusters):
+        """
+        Find the cluster or clusters with maximum parking slots.
+        """
+        max_points_clusters = list()
+        max_len = 0
+        # Get max lenth of clusters.
+        for cluster in clusters:
+            if len(cluster) > max_len:
+                max_len = len(cluster)
+        # Find clusters with max lengths.
+        max_points_clusters.append(
+            [cluster for cluster in clusters if len(cluster) == max_len])
+
+        return max_points_clusters
+
+    def _get_clusters_centroid(self, cluster):
+        """ Get specific cluster's coordinates. """
+        LOGGER.debug(f'CLUSTER AT CENT: {cluster}')
+        centroid = MultiPoint(cluster[0]).centroid
+        centroid = str(centroid).translate(
+            str.maketrans({'(': '', ')': ''})).split(' ')[1:]
+        centroid = self._sanitize_lists_numbers(centroid)
+        return centroid
+
+    def _get_clusters_distance(self, clusters_center):
+        """ Get specific cluster's distance from user point. """
+        LOGGER.debug(f'{clusters_center}, {self.user_location}')
+        distance = math.sqrt(
+            (clusters_center[0] - float(self.user_location[0]))**2 +
+            (clusters_center[1] - float(self.user_location[1]))**2)
+
+        return distance
+
+    @staticmethod
+    def _sanitize_lists_numbers(list_to_sanitize):
+        """ Limit lists floats to 2 decimals and return """
+        for index, number in enumerate(list_to_sanitize):
+            list_to_sanitize[index] = float("%.2f" % round(float(number), 2))
+
+        return list_to_sanitize
