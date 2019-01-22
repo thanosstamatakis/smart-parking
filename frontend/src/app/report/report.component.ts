0004eb81ef6f88f8f2ab4cf8468c09829dbec28e
thanosstamatakis <thanos.stamatakis@gmail.com>
2019-01-22 19:03:07 +0200

    Add chart.js library as test for drawing demand data.

--- frontend/src/app/report/report.component.ts
+++ frontend/src/app/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-report',
@@ -7,8 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
+  chart: any;
+
+  dataPoints : Object =[{x: 10,y: 20}, {x: 15,y: 10}]
+  labels = ['x','y'];
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    
+    console.log(this.dataPoints);
+
+    this.chart = new Chart(document.getElementById('lineChart'), {
+      type: 'line',
+      data: {
+        labels: this.labels,
+        datasets: [{
+          label: 'x',
+          data: this.dataPoints,
+          borderColor: "#ffcc00"
+        },]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }]
+        }}
+    });
+
+    console.log(this.chart);
+
+   }
 
 }
