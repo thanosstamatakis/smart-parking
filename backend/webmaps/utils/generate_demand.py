3b850d821cc582b16ca87f7d393d6ebf4310c642
gTsoulos <giorgos577@gmail.com>
2019-02-03 14:35:06 +0200

    Generate 24 random numbers instead of 23.

--- backend/webmaps/utils/generate_demand.py
+++ backend/webmaps/utils/generate_demand.py
@@ -4,7 +4,7 @@ import random as rand
 def generate_demand():
     """ Retrun a list of demands per hour """
     rand_demands = list()
-    for i in range(23):
+    for i in range(24):
         rand_demands.append(round(rand.random(), 2))
     
     return rand_demands
