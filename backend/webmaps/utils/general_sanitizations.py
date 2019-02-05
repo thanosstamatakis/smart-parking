8ba1caaf9d859af8c091a86e4472324b4bf07446
gTsoulos <giorgos577@gmail.com>
2019-02-05 18:28:35 +0200

    Fix point sanitization ultil module to handle centroid points.

--- backend/webmaps/utils/general_sanitizations.py
+++ backend/webmaps/utils/general_sanitizations.py
@@ -1,9 +1,18 @@
 """ Utility module containing functions for sanitization """
+import logging
+from config import CONFIGURATION
+
+LOGGER = CONFIGURATION.get_logger(__name__)
+
 
 def point_sanitization(point):
     """ Sanitize point from MultiGeo format to list """
+    LOGGER.debug(f'POINT SANI: {point}')
     point = str(point).translate(
-            str.maketrans({'(': '', ')': ''})).split(' ')[1:]
-    point[0], point[1] = point[1], point[0]
+        str.maketrans({'(': '', ')': ''})).split(' ')[0:]
+    if 'POINT'in point:
+        point.remove('POINT')
+        return point
+    point[0], point[1]=point[1], point[0]
 
     return point
