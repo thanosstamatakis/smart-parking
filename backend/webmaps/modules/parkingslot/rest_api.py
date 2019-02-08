0def57b180a1e38035ea49e4c4ff80461656e15a
GiorgosT97 <giorgos577@gmail.com>
2019-02-08 21:48:26 +0200

    Add time of day to parking slots api.
    Time of slots is added to parking slots api as a float required
    parameter in order to get the time information to check for
    availability. Also the other api parameters are changed to
    required.

--- backend/webmaps/modules/parkingslot/rest_api.py
+++ backend/webmaps/modules/parkingslot/rest_api.py
@@ -17,11 +17,16 @@ LOGGER = CONFIGURATION.get_logger(__name__)
 # Model for parsing arguments.
 user_location_model = NAMESPACE.parser()
 user_location_model.add_argument(
-    'longitude', type=float, help='Longitude of parking slot.')
+    'longitude', type=float, help='Longitude of parking slot.', required=True)
 user_location_model.add_argument(
-    'latitude', type=float, help='Latitude of parking slot.')
+    'latitude', type=float, help='Latitude of parking slot.', required=True)
 user_location_model.add_argument(
-    'radius', type=float, help= 'Radius of circle to look for parking.'
+    'radius', type=float, help='Radius of circle to look for parking.',
+    required=True
+)
+user_location_model.add_argument(
+    'time', type=float, help='Time of day the user wants to park.',
+    required=True
 )
 
 
@@ -34,13 +39,15 @@ class ParkingSlot(Resource):
     def get(self):
         """ Return all available parking slots """
         args = flask.request.args
+        LOGGER.debug(args['time'])
         try:
             user_location = [args['latitude'], args['longitude']]
         except KeyError as e:
             LOGGER.debug(e)
             return VALIDATION_NAMESPACE.USER_LOCATION_INPUT_ERROR
         # Response
-        response = lib.get_clusters(user_location, args['radius'])
+        response = lib.get_clusters(
+            user_location, args['radius'], args['time'])
         response = flask.jsonify(response)
 
         return response
